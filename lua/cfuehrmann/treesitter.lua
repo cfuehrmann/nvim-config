41769119a34db01a50d73a532817d585b5fbac9d
cfuehrmann <cfuhrmann@gmail.com>
2022-09-13 22:53:38 +0200

    Switch treesitter to moderate allow-list of languages
    To make initial install more bearable

--- lua/cfuehrmann/treesitter.lua
+++ lua/cfuehrmann/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = "all",
+  ensure_installed = { "bash", "c_sharp", "fish", "html", "javascript", "json", "markdown", "rust", "toml", "tsx", "typescript" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
