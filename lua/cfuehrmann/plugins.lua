c68ccefe8bda7a7c3423787e641db73f076b0b0e
cfuehrmann <cfuhrmann@gmail.com>
2022-09-29 22:48:28 +0200

    Add neogit and diffview

--- lua/cfuehrmann/plugins.lua
+++ lua/cfuehrmann/plugins.lua
@@ -47,6 +47,7 @@ return require('packer').startup(function(use)
     end
   }
 
+  use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
   use { 'sindrets/diffview.nvim', requires = { 'nvim-lua/plenary.nvim', "kyazdani42/nvim-web-devicons" } }
 
   -- Automatically set up your configuration after cloning packer.nvim
